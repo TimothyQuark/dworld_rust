d573bf4a292c77c87137478af8608f35ee20525d
TimothyQuark <>
2020-01-11 14:42:48 +0100

    DWorld branch which is entirely self built, using the amethyst game engine

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # dworld_rust
 
 A project to learn Rust, which follows the Roguelike Rust tutorial: http://bfnightly.bracketproductions.com/rustbook/chapter_0.html
+
+# dworld_rust
+
+## How to run
+
+To run the game, use
+
+```
+cargo run --features "vulkan"
+```
+
+on Windows and Linux, and
+
+```
+cargo run --features "metal"
+```
+
+on macOS.
+
+For building without any graphics backend, you can use
+
+```
+cargo run --features "empty"
+```
+
+but be aware that as soon as you need any rendering you won't be able to run your game when using
+the `empty` feature.
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,58 @@
-rltk::add_wasm_support!();
-use rltk::{Rltk, GameState, Console};
+use amethyst::{
+    core::transform::TransformBundle,
+    ecs::prelude::{ReadExpect, Resources, SystemData},
+    prelude::*,
+    renderer::{
+        plugins::{RenderFlat2D, RenderToWindow},
+        types::DefaultBackend,
+        RenderingBundle,
+    },
+    utils::application_root_dir,
+};
 
-struct State {}
-impl GameState for State {
-    fn tick(&mut self, ctx : &mut Rltk) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello Rust World");
-    }
+fn load_sprite_sheet(world: &mut World, png_path: &str, ron_path: &str) -> SpriteSheetHandle {
+    let texture_handle = {
+        let loader = world.read_resource::<Loader>();
+        let texture_storage = world.read_resource::<AssetStorage<Texture>>();
+        loader.load(png_path, ImageFormat::default(), (), &texture_storage)
+    };
+    let loader = world.read_resource::<Loader>();
+    let sprite_sheet_store = world.read_resource::<AssetStorage<SpriteSheet>>();
+    loader.load(
+        ron_path,
+        SpriteSheetFormat(texture_handle),
+        (),
+        &sprite_sheet_store,
+    )
 }
 
-fn main() {
-    let context = Rltk::init_simple8x8(80, 50, "Hello Rust World", "resources");
-    let gs = State{ };
-    rltk::main_loop(context, gs);
+struct InitialState;
+
+impl SimpleState for InitialState {
+    fn on_start(&mut self, _data: StateData<'_, GameData<'_, '_>>) {}
+}
+
+fn main() -> amethyst::Result<()> {
+    amethyst::start_logger(Default::default());
+
+    let app_root = application_root_dir()?;
+
+    let config_dir = app_root.join("config");
+    let display_config_path = config_dir.join("display.ron");
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(
+            RenderingBundle::<DefaultBackend>::new()
+                .with_plugin(
+                    RenderToWindow::from_config_path(display_config_path)
+                        .with_clear([0.34, 0.36, 0.52, 1.0]),
+                )
+                .with_plugin(RenderFlat2D::default()),
+        )?
+        .with_bundle(TransformBundle::new())?;
+
+    let mut game = Application::new("/", InitialState, game_data)?;
+    game.run();
+
+    Ok(())
 }
