6755f671c83dcbb14ae448a6db8840acedd50277
TimothyQuark <timm.anywhere@yahoo.com>
2020-07-27 11:20:13 +0200

    Added release profile settings, edited readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # dworld_rust
 
-A project to learn Rust, which follows the Roguelike Rust tutorial, but using tcod (libtcod) instead: http://bfnightly.bracketproductions.com/rustbook/chapter_0.html
+A project to learn Rust, which follows the Roguelike Rust tutorial: http://bfnightly.bracketproductions.com/rustbook/chapter_0.html
+
--- src/player.rs
+++ src/player.rs
@@ -65,34 +65,39 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) -> RunState {
     match ctx.key {
         None => return RunState::AwaitingInput, // Nothing happened
         Some(key) => match (key, shift_down, control_down) {
-
             // Diagonals. Checked first because of control and shift modifiers
-            (VirtualKeyCode::Right, true, false) |(VirtualKeyCode::Numpad9, ..) | (VirtualKeyCode::U, ..) => try_move_player(1, -1, &mut gs.ecs),
+            (VirtualKeyCode::Right, true, false)
+            | (VirtualKeyCode::Numpad9, ..)
+            | (VirtualKeyCode::U, ..) => try_move_player(1, -1, &mut gs.ecs),
 
-            (VirtualKeyCode::Left, true, false) |(VirtualKeyCode::Numpad7, ..) | (VirtualKeyCode::Y, ..) => try_move_player(-1, -1, &mut gs.ecs),
+            (VirtualKeyCode::Left, true, false)
+            | (VirtualKeyCode::Numpad7, ..)
+            | (VirtualKeyCode::Y, ..) => try_move_player(-1, -1, &mut gs.ecs),
 
-            (VirtualKeyCode::Right, false, true) | (VirtualKeyCode::Numpad3, ..) | (VirtualKeyCode::N, ..) => try_move_player(1, 1, &mut gs.ecs),
+            (VirtualKeyCode::Right, false, true)
+            | (VirtualKeyCode::Numpad3, ..)
+            | (VirtualKeyCode::N, ..) => try_move_player(1, 1, &mut gs.ecs),
 
-            (VirtualKeyCode::Left, false, true) |(VirtualKeyCode::Numpad1, ..) | (VirtualKeyCode::B, ..) => try_move_player(-1, 1, &mut gs.ecs),
+            (VirtualKeyCode::Left, false, true)
+            | (VirtualKeyCode::Numpad1, ..)
+            | (VirtualKeyCode::B, ..) => try_move_player(-1, 1, &mut gs.ecs),
 
             // Cardinal directions
-            (VirtualKeyCode::Left, ..) | (VirtualKeyCode::Numpad4, ..) | (VirtualKeyCode::H, ..) => {
-                try_move_player(-1, 0, &mut gs.ecs)
-            }
+            (VirtualKeyCode::Left, ..)
+            | (VirtualKeyCode::Numpad4, ..)
+            | (VirtualKeyCode::H, ..) => try_move_player(-1, 0, &mut gs.ecs),
 
-            (VirtualKeyCode::Right, ..) | (VirtualKeyCode::Numpad6, ..) | (VirtualKeyCode::L, ..) => {
-                try_move_player(1, 0, &mut gs.ecs)
-            }
+            (VirtualKeyCode::Right, ..)
+            | (VirtualKeyCode::Numpad6, ..)
+            | (VirtualKeyCode::L, ..) => try_move_player(1, 0, &mut gs.ecs),
 
-            (VirtualKeyCode::Up, ..) | (VirtualKeyCode::Numpad8,..) | (VirtualKeyCode::K, ..) => {
+            (VirtualKeyCode::Up, ..) | (VirtualKeyCode::Numpad8, ..) | (VirtualKeyCode::K, ..) => {
                 try_move_player(0, -1, &mut gs.ecs)
             }
 
-            (VirtualKeyCode::Down, ..) | (VirtualKeyCode::Numpad2, ..) | (VirtualKeyCode::J, ..) => {
-                try_move_player(0, 1, &mut gs.ecs)
-            }
-
-            
+            (VirtualKeyCode::Down, ..)
+            | (VirtualKeyCode::Numpad2, ..)
+            | (VirtualKeyCode::J, ..) => try_move_player(0, 1, &mut gs.ecs),
 
             _ => return RunState::AwaitingInput,
         },
