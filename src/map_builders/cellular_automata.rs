2338ff80077af056aa57502b0a1b226c2b286ebf
TimothyQuark <timm.anywhere@gmail.com>
2021-03-16 15:28:12 +0100

    Finished chapter 29

--- src/map_builders/cellular_automata.rs
+++ src/map_builders/cellular_automata.rs
@@ -1,13 +1,10 @@
-use std::usize;
-
 use super::{
-    apply_room_to_map, spawner, Map, MapBuilder, Position, Rect, TileType, SHOW_MAPGEN_VISUALIZER,
+    generate_voronoi_spawn_regions, remove_unreachable_areas_returning_most_distant, spawner, Map,
+    MapBuilder, Position, TileType, SHOW_MAPGEN_VISUALIZER,
 };
 use rltk::RandomNumberGenerator;
 use specs::prelude::*;
 use std::collections::HashMap;
-
-const MIN_ROOM_SIZE: i32 = 8;
 
 pub struct CellularAutomataBuilder {
     map: Map,
@@ -62,9 +59,11 @@ impl CellularAutomataBuilder {
         }
     }
 
-    pub fn build(&mut self) {
+    #[allow(clippy::map_entry)]
+    fn build(&mut self) {
         let mut rng = RandomNumberGenerator::new();
 
+        // First we completely randomize the map, setting 55% of it to be floor.
         for y in 1..self.map.height - 1 {
             for x in 1..self.map.width - 1 {
                 let roll = rng.roll_dice(1, 100);
@@ -78,6 +77,7 @@ impl CellularAutomataBuilder {
         }
         self.take_snapshot();
 
+        // Now we iteratively apply cellular automata rules
         for _i in 0..15 {
             let mut newtiles = self.map.tiles.clone();
 
@@ -122,6 +122,7 @@ impl CellularAutomataBuilder {
             self.take_snapshot();
         }
 
+        // Find a starting point; start at the middle and walk left until we find an open tile
         self.starting_position = Position {
             x: self.map.width / 2,
             y: self.map.height / 2,
@@ -135,55 +136,17 @@ impl CellularAutomataBuilder {
                 .map
                 .xy_idx(self.starting_position.x, self.starting_position.y);
         }
-
-        let map_starts: Vec<usize> = vec![start_idx];
-        let dijkstra_map = rltk::DijkstraMap::new(
-            self.map.width,
-            self.map.height,
-            &map_starts,
-            &self.map,
-            200.0,
-        );
-        let mut exit_tile = (0, 0.0f32);
-        for (i, tile) in self.map.tiles.iter_mut().enumerate() {
-            if *tile == TileType::Floor {
-                let distance_to_start = dijkstra_map.map[i];
-                // We can't get to this tile - so we'll make it a wall
-                if distance_to_start == std::f32::MAX {
-                    *tile = TileType::Wall;
-                } else {
-                    // If it is further away than our current exit candidate, move the exit
-                    if distance_to_start > exit_tile.1 {
-                        exit_tile.0 = i;
-                        exit_tile.1 = distance_to_start;
-                    }
-                }
-            }
-        }
         self.take_snapshot();
 
-        self.map.tiles[exit_tile.0] = TileType::DownStairs;
+        // Find all tiles we can reach from the starting point
+        let exit_tile = remove_unreachable_areas_returning_most_distant(&mut self.map, start_idx);
         self.take_snapshot();
 
-        let mut noise = rltk::FastNoise::seeded(rng.roll_dice(1, 65536) as u64);
-        noise.set_noise_type(rltk::NoiseType::Cellular);
-        noise.set_frequency(0.08);
-        noise.set_cellular_distance_function(rltk::CellularDistanceFunction::Manhattan);
+        // Place the stairs
+        self.map.tiles[exit_tile] = TileType::DownStairs;
+        self.take_snapshot();
 
-        for y in 1..self.map.height - 1 {
-            for x in 1..self.map.width - 1 {
-                let idx = self.map.xy_idx(x, y);
-                if self.map.tiles[idx] == TileType::Floor {
-                    let cell_value_f = noise.get_noise(x as f32, y as f32) * 10240.0;
-                    let cell_value = cell_value_f as i32;
-
-                    if self.noise_areas.contains_key(&cell_value) {
-                        self.noise_areas.get_mut(&cell_value).unwrap().push(idx);
-                    } else {
-                        self.noise_areas.insert(cell_value, vec![idx]);
-                    }
-                }
-            }
-        }
+        // Now we build a noise map for use in spawning entities later
+        self.noise_areas = generate_voronoi_spawn_regions(&self.map, &mut rng);
     }
 }
--- src/map_builders/mod.rs
+++ src/map_builders/mod.rs
@@ -13,6 +13,8 @@ mod common;
 use common::*;
 mod cellular_automata;
 use cellular_automata::CellularAutomataBuilder;
+mod drunkard;
+use drunkard::{DrunkSpawnMode, DrunkardSettings, DrunkardsWalkBuilder};
 
 pub trait MapBuilder {
     fn build_map(&mut self);
@@ -25,14 +27,15 @@ pub trait MapBuilder {
 
 pub fn random_builder(new_depth: i32) -> Box<dyn MapBuilder> {
     let mut rng = rltk::RandomNumberGenerator::new();
-    let builder = rng.roll_dice(1, 2);
-    //let builder = 1;
+    let builder = rng.roll_dice(1, 7);
     match builder {
         1 => Box::new(BspDungeonBuilder::new(new_depth)),
         2 => Box::new(BspInteriorBuilder::new(new_depth)),
         3 => Box::new(CellularAutomataBuilder::new(new_depth)),
+        4 => Box::new(DrunkardsWalkBuilder::open_area(new_depth)),
+        5 => Box::new(DrunkardsWalkBuilder::open_halls(new_depth)),
+        6 => Box::new(DrunkardsWalkBuilder::winding_passages(new_depth)),
         _ => Box::new(SimpleMapBuilder::new(new_depth)),
     }
 
-    
 }
