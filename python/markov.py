5b4d1845a61b1a9038d867b64d4dcd10067ca615
TimothyQuark <timm.anywhere@gmail.com>
2021-03-21 20:37:19 +0100

    Added Python files to gitignore, moved Markov functions and names to own files

---
+++ python/markov.py
@@ -0,0 +1,65 @@
+import random
+import sys
+
+###############################################################################
+# Markov Name model
+# A random name generator, by Peter Corbett
+# http://www.pick.ucam.org/~ptc24/mchain.html
+# This script is hereby entered into the public domain
+###############################################################################
+class Mdict:
+    def __init__(self):
+        self.d = {}
+    def __getitem__(self, key):
+        if key in self.d:
+            return self.d[key]
+        else:
+            raise KeyError(key)
+    def add_key(self, prefix, suffix):
+        if prefix in self.d:
+            self.d[prefix].append(suffix)
+        else:
+            self.d[prefix] = [suffix]
+    def get_suffix(self,prefix):
+        l = self[prefix]
+        return random.choice(l)  
+
+class MName:
+    """
+    A name from a Markov chain
+    """
+    def __init__(self, names, chainlen = 2):
+        """
+        Building the dictionary
+        """
+        if chainlen > 10 or chainlen < 1:
+            print("Chain length must be between 1 and 10, inclusive")
+            sys.exit(0)
+    
+        self.mcd = Mdict()
+        oldnames = []
+        self.chainlen = chainlen
+    
+        for l in names:
+            l = l.strip()
+            oldnames.append(l)
+            s = " " * chainlen + l
+            for n in range(0,len(l)):
+                self.mcd.add_key(s[n:n+chainlen], s[n+chainlen])
+            self.mcd.add_key(s[len(l):len(l)+chainlen], "\n")
+    
+    def New(self):
+        """
+        New name from the Markov chain
+        """
+        prefix = " " * self.chainlen
+        name = ""
+        suffix = ""
+        while True:
+            suffix = self.mcd.get_suffix(prefix)
+            if suffix == "\n" or len(name) > 9:
+                break
+            else:
+                name = name + suffix
+                prefix = prefix[1:] + suffix
+        return name.capitalize()
